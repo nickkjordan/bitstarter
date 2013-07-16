13437bf0ac74154f796c3b41be732e4abdf4a545
Nick Jordan <nkj7@case.edu>
2013-07-16 00:03:59 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -2,12 +2,14 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buf = fs.readFileSync(index.html); 
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html));
+  response.send(buf.toString('utf8')));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
